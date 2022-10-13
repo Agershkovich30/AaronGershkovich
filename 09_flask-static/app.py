06b3a2c30310a2521c20b41b58b7990cd2878cf2
Aaron Gershkovich <agershkovich30@stuy.edu>
2022-10-13 09:03:58 -0400

    added 09

---
+++ 09_flask-static/app.py
@@ -0,0 +1,22 @@
+'''
+Goda (Aaron Gershkovich,Julia Lee, Weichen Liu)
+SoftDev
+K09
+2022-10-04
+Time Spent: 1 hr
+'''
+# DEMO 
+# basics of /static folder
+
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
